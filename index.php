3ad5147dd10ab284f47e1dcf5b6674fec76f7660
Filip Siwinski <dev.siwy@gmail.com>
2024-12-18 12:27:37 +0100

    s

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+require ('app/config/session.php');
 header('Content-Type: text/html; charset=UTF-8');
 
 require('app/config/config.php');
@@ -6,24 +8,17 @@ require('app/config/db.php');
 require('app/functions/validate.function.php');
 require('app/functions/helper.function.php');
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-	fieldRequired('Imię', $_POST['name']);
-	fieldRequired('Nazwisko', $_POST['surname']);
-	fieldRequired('E-mail', $_POST['email']);
-	fieldRequired('Hasło', $_POST['password']);
-	if(!$isError) {
-		isEmail('E-mail', $_POST['email']);
-	}
-
-	if (!$isError) {
-		$password = md5(PASS_SALT . $_POST['password']);
-		$query = "INSERT INTO users SET user_name = '{$_POST['name']}', user_surname = '{$_POST['surname']}', user_email = '{$_POST['email']}', user_password = '$password'";
-		if ($db->query($query)) {
-			echo 'Data was inserted Successfully';
-		} else {
-			echo 'Data has not been inserted!';
-		}
-	}
+if (isset($_SESSION['user']))
+{
+    include ('templates/MasterPage.html.php');
+}
+else
+{
+    include ('templates/LoginPage.html.php');
+    //include ('templates/MasterPage.html.php');
 }
 
-include('templates/views/MasterPage.html.php');
+if (isset($_SESSION['message']))
+{
+    unset($_SESSION['message']);
+}
